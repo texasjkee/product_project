548f9e1f4e486fe2ba6bc3b0e49ee115d4eb7aeb
Michael <gt.gluck@gmail.com>
2023-12-08 15:25:57 +0200

    add: react-router-dom and lazy/suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { Counter } from "./components/Counter";
+import { Suspense } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 
 import './index.scss';
 
@@ -6,8 +10,14 @@ export const App = () => {
 
     return (
         <div className="app">
-        <span>App webpack</span>
-            <Counter />
+            <Link to={'/'}>Maing</Link>
+            <Link to={'/about'}> About</Link>
+            <Suspense fallback={<div>Loading ...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     )
 }
