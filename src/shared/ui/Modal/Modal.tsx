7e680d4e7b3a67bfd238668ccca03434203c83ba
Michael <gt.gluck@gmail.com>
2023-12-21 17:50:25 +0200

    add: Modal component

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,75 @@
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
+
+import { classNames } from "shared/lib/classNames/classNames";
+
+import style from "./Modal.module.scss";
+
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+};
+
+const ANIMATION_DELAY = 300;
+
+export const Modal = (props: ModalProps) => {
+    const {
+        className,
+        children,
+        isOpen,
+        onClose
+    } = props;
+
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
+    const handlerClose = useCallback(() => {
+        if (onClose) {
+            setIsClosing(true);
+            timerRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, ANIMATION_DELAY);
+        }
+    }, [onClose]);
+
+    const onKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            handlerClose();
+        }
+    }, [handlerClose]);
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown)
+        }
+
+        return () => {
+            clearTimeout(timerRef.current);
+        }
+    }, [isOpen]);
+
+    const onContentClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    const mods: Record<string, boolean> = {
+        [style.opened]: isOpen,
+        [style.isClosing]: isClosing,
+        // [style[theme]]: true,
+    }
+
+    return (
+        <div className={classNames(style.Modal, mods, [className])}>
+            <div className={style.overlay} onClick={handlerClose}>
+                <div
+                    className={style.content}
+                    onClick={onContentClick}
+                >
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+};
