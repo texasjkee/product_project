9cb50764007b1a6dcd4e2e4624b7ae0ae20da8ef
Michael <gt.gluck@gmail.com>
2023-12-21 13:01:18 +0200

    add: Button size & square

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ButtonHTMLAttributes, FC } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 
@@ -5,28 +6,47 @@ import style from "./Button.module.scss";
 
 export enum ThemeButton {
     CLEAR = 'clear',
+    OUTLINE = 'outline',
+    BACKGROUND = 'background',
+    BACKGROUND_INVERTED = 'backgroundInverted',
+}
+
+export enum ButtonSize {
+    M = 'size_m',
+    L = 'size_l',
+    XL = 'size_xl'
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
     theme?: ThemeButton;
-};
+    square?: boolean;
+    size?: ButtonSize;
+}
 
 export const Button: FC<ButtonProps> = (props) => {
     const {
         className,
         children,
         theme,
+        square,
+        size,
         ...otherProps
     } = props;
 
+    const mods: Record<string, boolean> = {
+        [style[theme]]: true,
+        [style.square]: square,
+        [style[size]]: true,
+    }
+
     return (
         <button
-            className={classNames(style.Button, { [style[theme]]: true }, [className])}
+            className={classNames(style.Button, mods, [className])}
             // className={classNames(style.Button, {}, [className, theme])}
             {...otherProps}
         >
             {children}
         </button>
     )
-};
+}
